b7d76ce2fb4b4321cd2f2b0add8042b28cc4dbe8
Leonardo168 <leonardo-96@hotmail.com.br>
2023-09-29 20:52:17 -0300

    Primeiro commit

---
+++ SegundaAPI/src/main/java/com/Leonardo168/api/SegundaApiApplication.java
@@ -0,0 +1,21 @@
+package com.Leonardo168.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SegundaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundaApiApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String index() {
+		return "Olá Mundo!";
+	}
+
+}
---
+++ SegundaAPI/src/test/java/com/Leonardo168/api/SegundaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Leonardo168.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
