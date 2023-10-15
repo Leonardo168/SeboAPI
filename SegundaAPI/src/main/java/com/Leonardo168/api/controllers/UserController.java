183afac668ecb028a2fc4f0459bd3a890e72325e
Leonardo168 <leonardo-96@hotmail.com.br>
2023-10-15 13:53:38 -0300

    Método DELETE implementado para /user

--- SegundaAPI/src/main/java/com/Leonardo168/api/controllers/UserController.java
+++ SegundaAPI/src/main/java/com/Leonardo168/api/controllers/UserController.java
@@ -9,7 +9,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -62,6 +64,19 @@ public class UserController {
 		userModel.setRoles(userModelOptional.get().getRoles());
 		userModel.setEnable(true);
 		return ResponseEntity.status(HttpStatus.OK).body(userService.save(userModel));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteUser(@PathVariable(value = "id") UUID id){
+		Optional<UserModel> userModelOptional = userService.findByID(id);
+		if(!userModelOptional.isPresent()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+		}
+		UserModel userModel = new UserModel();
+		BeanUtils.copyProperties(userModelOptional.get(), userModel);
+		userModel.setEnable(false);
+		userService.save(userModel);
+		return ResponseEntity.status(HttpStatus.OK).body("User deleted");
 	}
 
 }
--- SegundaAPI/src/main/java/com/Leonardo168/api/services/UserService.java
+++ SegundaAPI/src/main/java/com/Leonardo168/api/services/UserService.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import com.Leonardo168.api.models.ParkingSpotModel;
 import com.Leonardo168.api.models.UserModel;
 import com.Leonardo168.api.repositories.UserRepository;
 
@@ -31,6 +30,10 @@ public class UserService {
 	
 	public List<UserModel> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public Optional<UserModel> findByID(UUID id) {
+		return userRepository.findById(id);
 	}
 	
 	public Optional<UserModel> findByUsername(String username) {
